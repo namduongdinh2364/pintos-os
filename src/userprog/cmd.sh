5839b54c165352702deeb0fa23dda278e2f7cd4a
nam <namvkbn97edu@gmail.com>
2021-07-19 00:02:31 +0700

    Modify some file to work
    Signed-off-by: nam <namvkbn97edu@gmail.com>

---
+++ src/userprog/cmd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm filesys.dsk
+
+echo "Copy echo ELF to filesys.dsk"
+pintos-mkdisk filesys.dsk --filesys-size=2
+pintos -f -q
+pintos -p ../examples/echo -a echo -- -q
+echo "pintos run 'echo x'"
